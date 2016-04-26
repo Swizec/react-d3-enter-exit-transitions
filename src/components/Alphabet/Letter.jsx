42ad93ff1a1427113d1067748e68cfbf45af60a0
Swizec Teller <swizec@swizec.com>
2016-04-26 15:47:54 +0200

    Smooth side-to-side transitions

--- src/components/Alphabet/Letter.jsx
+++ src/components/Alphabet/Letter.jsx
@@ -3,21 +3,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import d3 from 'd3';
 
-// Gonna have to use this: http://stackoverflow.com/questions/29977799/how-should-i-handle-a-leave-animation-in-componentwillunmount-in-react
-
 class Letter extends Component {
     state = {
         y: -60,
         className: 'enter',
         fillOpacity: 1e-6
-    }
-
-    componentWillAppear(callback) {
-        //console.log('willAppear');
-    }
-
-    componentDidAppear() {
-        //console.log('didAppear');
     }
 
     componentWillEnter(callback) {
@@ -26,13 +16,8 @@ class Letter extends Component {
         node.transition()
             .duration(750)
             .attr('y', 0)
-            .style('fill-opacity', 1);
-
-        callback();
-    }
-
-    componentDidEnter() {
-        //console.log('didEnter');
+            .style('fill-opacity', 1)
+            .on('end', callback);
     }
 
     componentWillLeave(callback) {
@@ -47,51 +32,23 @@ class Letter extends Component {
             .on('end', callback);
     }
 
-    componentDidLeave() {
-        //console.log('didLeave');
-    }
-
-    /* text.attr("class", "update")
-       .attr("y", 0)
-       .style("fill-opacity", 1)
-       .transition(t)
-       .attr("x", (d, i) => i * 32); */
-
     componentWillReceiveProps(nextProps) {
-        if (this.props.old_i != nextProps.old_i) {
+        if (this.props.i != nextProps.i) {
             let node = d3.select(ReactDOM.findDOMNode(this));
 
             this.setState({className: 'update',
                            y: 0,
-                           fillOpacity: 1});
+                           fillOpacity: 1,
+                           old_i: this.props.i});
 
             node.transition()
                 .duration(750)
                 .attr('x', nextProps.i*32);
         }
-
     }
 
-    /* ext.exit()
-       .attr("class", "exit")
-       .transition(t)
-       .attr("y", 60)
-       .style("fill-opacity", 1e-6)
-       .remove(); */
-
-    /* componentWillUnmount() {
-       let node = d3.select(ReactDOM.findDOMNode(this));
-
-       this.setState({className: 'exit'});
-
-       node.transition()
-       .duration(750)
-       .attr('y', 60)
-       .style('fill-opacity', 1e-6);
-       } */
-
     render() {
-        let x = this.props.old_i ? this.props.old_i*32 : this.props.i*32;
+        let x = this.state.old_i ? this.state.old_i*32 : this.props.i*32;
 
         return (
             <text dy=".35em"
--- src/components/Alphabet/index.jsx
+++ src/components/Alphabet/index.jsx
@@ -12,64 +12,12 @@ class Alphabet extends Component {
     alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
     state = {letters: []}
 
-    componentDidUpdate() { this.update(this.state.letters) }
     componentDidMount() {
-        this.update(this.state.letters);
-
-        d3.interval(() => this.updateLetters(), 1500);
-    }
-
-    updateLetters() {
-        let oldLetters = this.state.letters,
-            newLetters = d3.shuffle(this.alphabet)
-                           .slice(0, Math.floor(Math.random() * this.alphabet.length))
-                           .sort(),
-            oldIndexes = {};
-
-        oldLetters.forEach((l, i) => oldIndexes[l] = i);
-
-        this.setState({
-            letters: newLetters,
-            oldIndexes: oldIndexes
-        })
-    }
-
-    update(data) {
-        let t = d3.transition()
-                  .duration(750);
-
-        let svg = d3.select(ReactDOM.findDOMNode(this));
-
-        // JOIN new data with old elements
-        let text = svg.selectAll("text")
-                      .data(data, (d) => d);
-
-        // EXIT old elements not present in new data
-        /* text.exit()
-           .attr("class", "exit")
-           .transition(t)
-           .attr("y", 60)
-           .style("fill-opacity", 1e-6)
-           .remove();
-
-           // UPDATE old elements present in new data
-           text.attr("class", "update")
-           .attr("y", 0)
-           .style("fill-opacity", 1)
-           .transition(t)
-           .attr("x", (d, i) => i * 32);
-
-           // ENTER new elements present in new data
-           text.enter().append("text")
-           .attr("class", "enter")
-           .attr("dy", ".35em")
-           .attr("y", -60)
-           .attr("x", (d, i) => i * 32)
-           .style("fill-opacity", 1e-6)
-           .text((d) => d)
-           .transition(t)
-           .attr("y", 0)
-           .style("fill-opacity", 1); */
+        d3.interval(() => this.setState({
+            letters: d3.shuffle(this.alphabet)
+                       .slice(0, Math.floor(Math.random() * this.alphabet.length))
+                       .sort()
+        }), 1500);
     }
 
     render() {
@@ -78,10 +26,9 @@ class Alphabet extends Component {
         return (
             <g transform={transform}>
                 <ReactTransitionGroup component="g">
-                {this.state.letters.map((d, i) => (
-                    <Letter d={d} i={i}
-                            old_i={this.state.oldIndexes[d]} key={`letter-${i}`} />
-                 ))}
+                    {this.state.letters.map((d, i) => (
+                        <Letter d={d} i={i} key={`letter-${d}`} />
+                     ))}
                 </ReactTransitionGroup>
             </g>
         );
